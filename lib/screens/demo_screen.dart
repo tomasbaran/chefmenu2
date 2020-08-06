3eddb3d95fbd8dbd6323b7dba4e204e8b3768004
Tomas iMac <tomas.baran@gmail.com>
2020-08-06 18:15:18 +0100

    Performance optimization: less rebuilds thanks to ChangeNotifierProvider

--- lib/screens/demo_screen.dart
+++ lib/screens/demo_screen.dart
@@ -4,104 +4,59 @@ import 'package:chefmenu2/theme/style_constants.dart';
 import 'package:flutter/material.dart';
 import 'package:chefmenu2/widgets/my_tab_bar.dart';
 import 'package:chefmenu2/widgets/sliver_app_bar_layer.dart';
+import 'package:provider/provider.dart';
 
-class DemoScreen extends StatefulWidget {
-  static String id = '/demo';
+class ScrollPosition extends ChangeNotifier {
+  double data = 0;
 
-  @override
-  _DemoScreenState createState() => _DemoScreenState();
+  updateData(double newData) {
+    data = newData;
+    notifyListeners();
+  }
 }
 
-class _DemoScreenState extends State<DemoScreen> {
-  double scrollPosition = 0;
+class DemoScreen extends StatelessWidget {
+  static String id = '/demo';
 
-  double calcContraints(dynamic context) => MediaQuery.of(context).size.height * kCoverHeightProportion - kBigBoxPadding;
+  final ScrollPosition scrollPosition = ScrollPosition();
 
-  double topPadding() => kCoverHeightProportion * MediaQuery.of(context).size.height - 50 - kShadowBlur;
+  //double topPadding() => kCoverHeightProportion * MediaQuery.of(context).size.height - 50 - kShadowBlur;
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 25,
-      child: Scaffold(
-        backgroundColor: colorBackground,
-        //floatingActionButton: MyTabBar(),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        body: NotificationListener<ScrollNotification>(
-          onNotification: (notification) {
-            //print('A: ${notification.metrics.viewportDimension}');//645
-            //print('E1: ${notification.metrics.extentBefore}'); //Before Pixel
-            //print('E2: ${notification.metrics.extentInside}');//645
-            //print('E3: ${notification.metrics.extentAfter}'); //Still left to the end of Pixel
-            // print('START: ${calcContraints(context)}');
-            print('P: ${notification.metrics.pixels}');
-            //setState(() {
-            scrollPosition = notification.metrics.pixels;
-            //});
+      child: ChangeNotifierProvider<ScrollPosition>(
+        create: (context) => scrollPosition,
+        child: Scaffold(
+          backgroundColor: colorBackground,
+          //floatingActionButton: MyTabBar(),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+          body: NotificationListener<ScrollNotification>(
+            onNotification: (notification) {
+              //print('A: ${notification.metrics.viewportDimension}');//645
+              //print('E1: ${notification.metrics.extentBefore}'); //Before Pixel
+              //print('E2: ${notification.metrics.extentInside}');//645
+              //print('E3: ${notification.metrics.extentAfter}'); //Still left to the end of Pixel
+              // print('START: ${calcContraints(context)}');
+              //print('P: ${notification.metrics.pixels}');
+              //setState(() {
+              scrollPosition.updateData(notification.metrics.pixels);
+              //});
 
-            return true;
-          },
-          child: Stack(
-            children: [
-              // CoverContainer(
-              //     scrollPosition: scrollPosition,
-              //     topPadding: kCoverHeightProportion * MediaQuery.of(context).size.height - 50 - kShadowBlur,
-              //     imageSrc: 'icons/cover.jpeg'),
-
-              Stack(children: [
-                BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
-                  child: Container(
-                    width: double.infinity,
-                    color: colorBackground.withOpacity(0),
-                    child: Image.asset(
-                      'icons/cover.jpeg',
-                      fit: BoxFit.cover,
-                      height: scrollPosition < calcContraints(context)
-                          ? MediaQuery.of(context).size.height * kCoverHeightProportion
-                          : MediaQuery.of(context).size.height * kCoverHeightProportion + scrollPosition - 244,
-                      isAntiAlias: true,
-                    ),
-                    //color: Colors.green,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: topPadding(), left: kBigBoxPadding),
-                  child: Text(
-                    '  ' + 'La casa de don Juan' + '  ',
-                    style: ktsRestaurantTitle,
-                  ),
-                ),
-                ScrollText(scrollPosition: scrollPosition),
-              ]),
-              CtaButton(),
-              BigBoxContainer(),
-            ],
+              return true;
+            },
+            child: Stack(
+              children: [
+                CoverContainer(
+                    topPadding: kCoverHeightProportion * MediaQuery.of(context).size.height - 50 - kShadowBlur, imageSrc: 'icons/cover.jpeg'),
+                CtaButton(),
+                BigBoxContainer(),
+              ],
+            ),
           ),
         ),
       ),
-    );
-  }
-}
-
-class ScrollText extends StatefulWidget {
-  const ScrollText({
-    Key key,
-    @required this.scrollPosition,
-  }) : super(key: key);
-
-  final double scrollPosition;
-
-  @override
-  _ScrollTextState createState() => _ScrollTextState();
-}
-
-class _ScrollTextState extends State<ScrollText> {
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      widget.scrollPosition.toString(),
-      style: TextStyle(color: Colors.white),
     );
   }
 }
@@ -145,41 +100,39 @@ class BigBoxContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return NotificationListener<ScrollNotification>(
-      child: Container(
-        margin: EdgeInsets.only(left: kBigBoxPadding, right: kBigBoxPadding, top: kBigBoxPadding, bottom: kBottomBigBoxPadding),
-        decoration: BoxDecoration(
-          //color: Colors.pink,
-          borderRadius: BorderRadius.all(Radius.circular(30)),
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.all(Radius.circular(30)),
-          child: CustomScrollView(
-            anchor: computeAnchor(context),
-            slivers: [
-              SliverAppBarLayer(
-                child: Container(
-                  width: double.infinity,
-                  height: kSliverAppBarLayerHeight,
-                  decoration: BoxDecoration(
-                      color: colorBackground, borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30))),
-                  child: Center(
-                    child: Text(
-                      'Entrantes',
-                      style: ktsCategoryTitle,
-                    ),
+    return Container(
+      margin: EdgeInsets.only(left: kBigBoxPadding, right: kBigBoxPadding, top: kBigBoxPadding, bottom: kBottomBigBoxPadding),
+      decoration: BoxDecoration(
+        //color: Colors.pink,
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+        child: CustomScrollView(
+          anchor: computeAnchor(context),
+          slivers: [
+            SliverAppBarLayer(
+              child: Container(
+                width: double.infinity,
+                height: kSliverAppBarLayerHeight,
+                decoration: BoxDecoration(
+                    color: colorBackground, borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30))),
+                child: Center(
+                  child: Text(
+                    'Entrantes',
+                    style: ktsCategoryTitle,
                   ),
                 ),
               ),
-              SliverGrid.extent(
-                maxCrossAxisExtent: kMaxCrossAxisExtent,
-                childAspectRatio: 1,
-                mainAxisSpacing: 0,
-                crossAxisSpacing: 0,
-                children: _buildGridTileList(context, 250),
-              ),
-            ],
-          ),
+            ),
+            SliverGrid.extent(
+              maxCrossAxisExtent: kMaxCrossAxisExtent,
+              childAspectRatio: 1,
+              mainAxisSpacing: 0,
+              crossAxisSpacing: 0,
+              children: _buildGridTileList(context, 250),
+            ),
+          ],
         ),
       ),
     );
@@ -210,11 +163,10 @@ class CtaButton extends StatelessWidget {
 class CoverContainer extends StatelessWidget {
   final double topPadding;
   final String imageSrc;
-  final double scrollPosition;
 
   double calcContraints(dynamic context) => MediaQuery.of(context).size.height * kCoverHeightProportion - kBigBoxPadding;
 
-  CoverContainer({this.imageSrc, this.topPadding, this.scrollPosition});
+  CoverContainer({this.imageSrc, this.topPadding});
 
   @override
   Widget build(BuildContext context) {
@@ -227,9 +179,9 @@ class CoverContainer extends StatelessWidget {
           child: Image.asset(
             imageSrc,
             fit: BoxFit.cover,
-            height: scrollPosition < calcContraints(context)
+            height: Provider.of<ScrollPosition>(context).data < calcContraints(context)
                 ? MediaQuery.of(context).size.height * kCoverHeightProportion
-                : MediaQuery.of(context).size.height * kCoverHeightProportion + scrollPosition - 244,
+                : MediaQuery.of(context).size.height * kCoverHeightProportion + Provider.of<ScrollPosition>(context).data - 244,
             isAntiAlias: true,
           ),
           //color: Colors.green,
@@ -242,10 +194,10 @@ class CoverContainer extends StatelessWidget {
           style: ktsRestaurantTitle,
         ),
       ),
-      // Text(
-      //   scrollPosition.toString(),
-      //   style: TextStyle(color: Colors.white),
-      // ),
+      Text(
+        Provider.of<ScrollPosition>(context).data.toString(),
+        style: TextStyle(color: Colors.white),
+      ),
     ]);
   }
 }
